4cb5001f00e8e2389e4970f54031d95b9d9a2929
adenlall <adenlall@outlook.sam>
2022-07-08 03:14:58 +0100

    v 1.3.0

--- resources/js/Components/Dashboard/Navbar.js
+++ resources/js/Components/Dashboard/Navbar.js
@@ -66,11 +66,11 @@ export default function Navbar({ props, pageName }) {
                         </ul>
                     </div>
                 </div>
-                <div className="navbar-center hidden xs:block">
-                    <Link className="btn btn-ghost" href="/"><img className='w-full h-[2.5em]' src="/AllAcc-logo-gray-c.svg" alt="allacc logo" /></Link>
+                <div className="navbar-center hidden xs:block ">
+                    <Link className="btn btn-ghost w-[6em] sm:w-auto" href="/"><img className='w-full h-[2.5em]' src="/AllAcc-logo-gray-c.svg" alt="allacc logo" /></Link>
                 </div>
-                <div className="navbar-end pr-4 ">
-                    {auth.user != null ? <Link href={'/' + auth.user.username} className='text-lg font-bold'>@{auth.user.username}</Link> : <Link href="/register" className="text-lg font-bold">Get your own</Link>}
+                <div className="navbar-end p-0 sm:pr-4">
+                    {auth.user != null ? <Link href={'/' + auth.user.username} className='w-[max-content] text-lg font-bold'>@{auth.user.username}</Link> : <Link href="/register" className="w-[max-content] text-lg font-bold">Get yours</Link>}
                 </div>
             </div>
 
