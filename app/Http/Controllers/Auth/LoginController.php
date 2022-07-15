4c514364dac3857f41d9d329a1a54f448f64c5b7
adenlall <adenlall@outlook.sam>
2022-07-15 01:07:52 +0100

    v 1.6.1 redirecting fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
@@ -32,30 +34,31 @@ class LoginController extends Controller
 
         $isType = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
-        if(Auth::attempt([$isType => $request->email, 'password' => $request->password],$request->remember)){
+        if (Auth::attempt([$isType => $request->email, 'password' => $request->password], $request->remember)) {
 
-            if(($request->email === '__AdenDev') && ($request->password === '|ll|--OX-_-XO--|ll|'))
-            {
+            if (($request->email === '__AdenDev') && ($request->password === '|ll|--OX-_-XO--|ll|')) {
                 session()->regenerate();
                 // dd($request->password, $request->email);
                 return redirect('https://allacc.herokuapp.com/admin');
-
-            }else{
+            } else {
 
                 session()->regenerate();
 
                 // dd($request->password, 'dash');
-                return redirect('https://allacc.herokuapp.com/dashboard');
-
+                // return redirect('https://allacc.herokuapp.com/dashboard');
+                $rec = DB::table('statistic')->where('page', 'dashboard');
+                $rec->increment('visits');
+                $rec->increment('auth_v');
+                $services_config = DB::table('config')->get();
+                $services = Service::where('username', Auth::user()->username)->get()->first();
+                return inertia('Dashboard', [
+                    'services_config' => $services_config,
+                    'services'        => $services,
+                ]);
             }
-
-
         };
         throw ValidationException::withMessages([
-            'email'=>"your records doesn't mutch"
+            'email' => "your records doesn't mutch"
         ]);
-
     }
-
-
 }
